b8a6fbea26801d2b11aff2ac27b211b2e1ef454e
Provida Powell <102064281+Vida-1@users.noreply.github.com>
2022-03-25 15:59:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,35 +10,24 @@ function getAnswer(){
               }
 }
 
+// this code was written by John Cokos and is run here as a demonstration, not a representation of my own work
 
+function guessNumber() {
+        let answer;
+        let guesses = 0;
 
+        do {
+        guesses++;
+        answer=prompt("Guess a number between 1 and 10");
+        if (parseInt(answer) ===7) {
+                document.write("It took you " +guesses + " times to guess my number")
+        } 
 
+        if (parseInt(answer) === 7)  {
+                document.write ("It took you " + guesses + " times to guess my number")
+        }
 
-// Notes from class today follow:
+}
 
-// function yourName() {
-
-//         let name = prompt ("What is your name?");
-//             document.write("Nice to meet you, " + name);
-// } 
-
-
-// function guessNumber() {
-//         let answer;
-//         let guesses = 0;
-
-//         do {
-//         guesses++;
-//         answer=prompt("Guess a number between 1 and 10");
-//         if (parseInt(answer) ===7) {
-//                 document.write("It took you " +guesses + " times to guess my number")
-//         } 
-
-//         if (parseInt(answer) === 7)  {
-//                 document.write ("It took yu " + guesses + " times to guess my number")
-//         }
-
-// }
-
-// } 
+} 
 
