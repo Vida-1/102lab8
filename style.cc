ae0fb00516dc2c31e95a9f5f8e87f6fc8fb946da
Provida Powell <102064281+Vida-1@users.noreply.github.com>
2022-03-25 15:49:24 -0400

    Update style.cc

--- style.cc
+++ style.cc
@@ -1,1 +1,71 @@
+body ,html{
+    Align: justify;
+    max-width: 1100px;
+    background-color: rgb(180, 179, 179);
+    margin: 5px;
+    padding:5px;
+  }
+
+header{
+    background-color:#ccc;
+    display: block;
+    }
+
+        header img{
+            box-align:left;
+            padding: 10px;
+            float: left;
+        }
+
+        header iframe{
+            box-align:left;
+            padding: 10px;
+            float: left;
+        }
+
+        header h1{
+            display:inline-block;
+            padding: 10px;
+        }
+        
+        header h2{
+            display:inline-block;
+            padding: 5px 0px 0px 10px;
+            font-style:italic;
+            color: rgb(0,0,255);
+        }
+            
+        header h3{
+            display:block;
+            padding:15px;
+            margin: 15px;
+        }
+        
+          header ul li{
+              display:inline-block;
+              padding: 0px 70px 0px 20px;
+              margin: 0px 20px 0px 0px;
+              font-style: italic;
+          }
+main {
+    background-color: #ccc;
+     }
+
+    section.left img {
+        padding: 0px;
+        margin: 10px;
+        display: block;
+        float: left;
+    }
+    
+    section.right img {
+        display:block;
+        float: right;
+        margin:10px;
+    }
+
+    main p{
+        padding:10px;
+        text-align:justify;
+    }
 
